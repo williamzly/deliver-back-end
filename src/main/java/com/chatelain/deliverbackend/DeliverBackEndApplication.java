da3b8fb4579157b615ef3b2917a3a507636626e8
william <4269946@qq.com>
2020-02-05 23:48:05 +0800

    first commit

---
+++ src/main/java/com/chatelain/deliverbackend/DeliverBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.chatelain.deliverbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliverBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chatelain/deliverbackend/DeliverBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatelain.deliverbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
